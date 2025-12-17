51fcbc267786953e68d6ead647f8b59390b2aaf1
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 20:30:03 +0200

    Implement Elm routing and basic pages

---
+++ src/Main.elm
@@ -0,0 +1,143 @@
+module Main exposing (..)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (Html, a, div, h1, header, main_, nav, text)
+import Html.Attributes exposing (..)
+import Url
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    , route : Route
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url (parseUrl url), Cmd.none )
+
+
+-- ROUTE
+
+
+type Route
+    = Home
+    | EventDetail String
+    | EditEvent String
+    | Callback
+    | NotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ Parser.map Home Parser.top
+        , Parser.map EventDetail (s "events" </> string)
+        , Parser.map EditEvent (s "events" </> string </> s "edit")
+        , Parser.map Callback (s "callback")
+        ]
+
+
+parseUrl : Url.Url -> Route
+parseUrl url =
+    case Parser.parse routeParser url of
+        Just route ->
+            route
+
+        Nothing ->
+            NotFound
+
+
+-- UPDATE
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url, route = parseUrl url }
+            , Cmd.none
+            )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Event Calendar"
+    , body =
+        [ div []
+            [ header []
+                [ nav []
+                    [ a [ href "/" ] [ text "Home" ]
+                    , text " | "
+                    , a [ href "/events/123" ] [ text "Event Detail" ]
+                    , text " | "
+                    , a [ href "/events/123/edit" ] [ text "Edit Event" ]
+                    , text " | "
+                    , a [ href "/callback" ] [ text "Callback" ]
+                    ]
+                ]
+            , main_ []
+                [ case model.route of
+                    Home ->
+                        h1 [] [ text "Home/Dashboard Page" ]
+
+                    EventDetail id ->
+                        h1 [] [ text ("Event Detail Page for " ++ id) ]
+
+                    EditEvent id ->
+                        h1 [] [ text ("Edit Event Page for " ++ id) ]
+
+                    Callback ->
+                        h1 [] [ text "Auth Callback Page" ]
+
+                    NotFound ->
+                        h1 [] [ text "Not Found" ]
+                ]
+            ]
+        ]
+    }
